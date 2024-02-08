ae4032c856cb3e031e44deba707cc1cf0e0e97fb
Pawan Gupta <61946159+PawanGupta17@users.noreply.github.com>
2024-02-08 19:46:30 +0530

    Update server.js

--- main/server.js
+++ main/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import stripe from "stripe";
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 //Load variables
 dotenv.config();
