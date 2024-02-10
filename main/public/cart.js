d8dfee02c5ff601192ec18245c5925dc641364d6
Pawan Gupta <61946159+PawanGupta17@users.noreply.github.com>
2024-02-10 11:26:16 +0530

    Update cart.js

--- main/public/cart.js
+++ main/public/cart.js
@@ -1,4 +1,38 @@
+import express from "express";
+
 const payBtn = document.querySelector(".white.btn-buy");
+const app = express();
+
+app.post("/stripe-checkout", async (req, res) => {
+  const lineItems = req.body.items.map((item) => {
+    const unitAmount = parseInt(item.price.replace(/[^0-9.-]+/g, "") * 100);
+    console.log("PG item-price:", item.price);
+    console.log("unitAmount:", unitAmount);
+    return {
+      price_data: {
+        currency: "USD",
+        product_data: {
+          name: item.title,
+          images: [item.productImg],
+        },
+        unit_amount: unitAmount,
+      },
+      quantity: item.quantity,
+    };
+  });
+  console.log("lineItems:", lineItems);
+  //create Checkout Session
+  const session = await stripeGateway.checkout.sessions.create({
+    payment_method_types: ["card"],
+    mode: "payment",
+    success_url: `${DOMAIN}/success`,
+    cancel_url: `${DOMAIN}/cancel`,
+    line_items: lineItems,
+    //Asking Address In Stripe Checkout
+    billing_address_collection: "required",
+  });
+  res.json(session.url);
+});
 
 payBtn.addEventListener("click", () => {
   console.log("PG");
